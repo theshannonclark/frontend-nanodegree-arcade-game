aa13cf0dba32a9d6cbdf7b4c47e9ca1cda5cf32d
Shannon Clark <theshannonclark@gmail.com>
2018-02-13 00:36:50 +1100

    Fix indentation issue

--- js/engine/tiledmap.js
+++ js/engine/tiledmap.js
@@ -16,20 +16,20 @@ TiledMap.prototype.initMap = function(firstScreen) {
 TiledMap.prototype.generateRow = function() {};
 
 TiledMap.prototype.render = function() {
-    // Before drawing, clear existing canvas
-    ctx.clearRect(0, 0, Engine.canvas.width, Engine.canvas.height)
+  // Before drawing, clear existing canvas
+  ctx.clearRect(0, 0, Engine.canvas.width, Engine.canvas.height)
 
-    // Draw the map
-    for (let row = 0; row < this.rows; row++) {
-      let rowImage = Resources.get(this._map[row]);
+  // Draw the map
+  for (let row = 0; row < this.rows; row++) {
+    let rowImage = Resources.get(this._map[row]);
 
-      for (let column = 0; column < this.columns; column++) {
-        let dx = column * this.tileWidth;
-        let dy = row * this.tileHeight;
+    for (let column = 0; column < this.columns; column++) {
+      let dx = column * this.tileWidth;
+      let dy = row * this.tileHeight;
 
-        ctx.drawImage(rowImage, dx, dy);
-      }
+      ctx.drawImage(rowImage, dx, dy);
     }
+  }
 };
 
 TiledMap.prototype.canStand = function(x, y) {};
